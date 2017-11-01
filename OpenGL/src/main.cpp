83983475eb047ad143514a54491eb3503110bda9
AustinCarter <austin@aktri.com>
2017-11-01 17:16:13 -0400

    adding transparencies, model is jumping

--- OpenGL/src/main.cpp
+++ OpenGL/src/main.cpp
@@ -16,6 +16,7 @@ void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
+unsigned int loadTexture(char const * path);
 
 // settings
 const unsigned int SCR_WIDTH = 800;
@@ -75,12 +76,58 @@ int main()
 
 	glEnable(GL_DEPTH_TEST);
 
+	glEnable(GL_STENCIL_TEST);
+    glStencilFunc(GL_NOTEQUAL, 1, 0xFF);
+    glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE);
+
+    glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+
+	vector<glm::vec3> vegetation;
+	vegetation.push_back(glm::vec3(-1.5f,  0.0f, -0.48f));
+	vegetation.push_back(glm::vec3( 1.5f,  0.0f,  0.51f));
+	vegetation.push_back(glm::vec3( 0.0f,  0.0f,  0.7f));
+	vegetation.push_back(glm::vec3(-0.3f,  0.0f, -2.3f));
+	vegetation.push_back(glm::vec3( 0.5f,  0.0f, -0.6f));
+
+	
+    //locations to draw windows
+    float transparentVertices[] = {
+        // positions         // texture Coords (swapped y coordinates because texture is flipped upside down)
+        0.0f,  0.5f,  0.0f,  0.0f,  0.0f,
+        0.0f, -0.5f,  0.0f,  0.0f,  1.0f,
+        1.0f, -0.5f,  0.0f,  1.0f,  1.0f,
+
+        0.0f,  0.5f,  0.0f,  0.0f,  0.0f,
+        1.0f, -0.5f,  0.0f,  1.0f,  1.0f,
+        1.0f,  0.5f,  0.0f,  1.0f,  0.0f
+    };
+
 	//std::cout << glGetString(GL_VERSION) << std::endl;
 
 	Shader ourShader("resources/shader.vert", "resources/shader.frag"); //load and compile shaders
+	Shader outlineShader("resources/outlineShader.vert", "resources/outlineShader.frag"); //load and compile shaders for outlining model
+	Shader transShader("resources/transShader.vert", "resources/transShader.frag"); //load and compile shaders for outlining model
 	//Shader lampShader("resources/lightShader.vert", "resources/lightShader.frag"); //load and compile light shaders
 
 	Model ourModel("resources/nanosuit/nanosuit.obj");
+	//load window texture
+	unsigned int windowTexture = loadTexture("resources/window.png");
+
+	unsigned int VAO;
+	unsigned int VBO;
+	glGenVertexArrays(1,&VAO);
+	glGenBuffers(1,&VBO);
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(transparentVertices), transparentVertices, GL_STATIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), 0);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)(3 * sizeof(float)));
+	glBindVertexArray(0);
+
 
 	// render loop
 	while (!glfwWindowShouldClose(window))
@@ -96,10 +143,16 @@ int main()
 
 		// render
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT| GL_DEPTH_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT| GL_DEPTH_BUFFER_BIT|GL_STENCIL_BUFFER_BIT);
+       
 
+        glStencilFunc(GL_ALWAYS, 1, 0xFF);
+        glStencilMask(0xFF);
 
 		ourShader.use();
+
+		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+        glm::mat4 view = camera.GetViewMatrix();
 
 		//pointLight 
 		//
@@ -124,23 +177,73 @@ int main()
         ourShader.setFloat("spotLight.cutOff", glm::cos(glm::radians(12.5f)));
         ourShader.setFloat("spotLight.cutOffOuter", glm::cos(glm::radians(17.5f)));   
 
+
         ourShader.setVec3("viewPos", camera.Position);
         ourShader.setFloat("shininess", 32.0f); 
 
        // view/projection transformations
-        glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-        glm::mat4 view = camera.GetViewMatrix();
+        
         ourShader.setMat4("projection", projection);
         ourShader.setMat4("view", view);
 
         // render the loaded model
-        glm::mat4 model;
-        model = glm::translate(model, glm::vec3(0.0f, -1.75f, 0.0f)); // translate it down so it's at the center of the scene
+        glm::mat4 model = glm::translate(model, glm::vec3(0.0f, -1.75f, 0.0f)); // translate it down so it's at the center of the scene
         model = glm::scale(model, glm::vec3(0.2f, 0.2f, 0.2f));	// it's a bit too big for our scene, so scale it down
         ourShader.setMat4("model", model);
         ourModel.Draw(ourShader);
 
 
+       outlineShader.use();
+
+        //second render pass to outline model in red
+        glStencilFunc(GL_NOTEQUAL, 1, 0xFF);
+        glStencilMask(0x00);
+        glDisable(GL_DEPTH_TEST);
+        float scale = 1.01;
+
+        model = glm::scale(model, glm::vec3(scale, scale, scale));
+
+        outlineShader.setMat4("model",model);
+        outlineShader.setMat4("projection", projection);
+        outlineShader.setMat4("view", view);
+
+        ourModel.Draw(outlineShader);
+
+         glStencilMask(0xFF);
+        glEnable(GL_DEPTH_TEST);
+
+        glStencilFunc(GL_ALWAYS, 1, 0xFF);
+        glStencilMask(0xFF);
+        glEnable(GL_DEPTH_TEST);
+
+        transShader.use();
+
+ 		//transShader.setMat4("model",model);
+        transShader.setMat4("projection", projection);
+        transShader.setMat4("view", view);
+
+        transShader.setInt("texture1",0);
+
+		std::map<float, glm::vec3> sorted;
+		for (unsigned int i = 0; i < vegetation.size(); i++)
+		{
+			float distance = glm::length(camera.Position - vegetation[i]);
+			sorted[distance] = vegetation[i];
+		}
+
+
+        glBindVertexArray(VAO);
+        glBindTexture(GL_TEXTURE_2D, windowTexture);
+		for(std::map<float,glm::vec3>::reverse_iterator it = sorted.rbegin(); it != sorted.rend(); ++it) 
+		{
+		    model = glm::mat4();
+		    model = glm::translate(model, it->second);				
+		    transShader.setMat4("model", model);
+		    glDrawArrays(GL_TRIANGLES, 0, 6);
+		}  
+		glBindVertexArray(0);
+
+       
 
         //swap buffers look for key/mouse/etc. events
 		glfwSwapBuffers(window);
@@ -150,6 +253,43 @@ int main()
 	// glfw: terminate, clearing all previously allocated GLFW resources.
 	glfwTerminate();
 	return 0;
+}
+
+unsigned int loadTexture(char const * path)
+{
+    unsigned int textureID;
+    glGenTextures(1, &textureID);
+
+    int width, height, nrComponents;
+    unsigned char *data = stbi_load(path, &width, &height, &nrComponents, 0);
+    if (data)
+    {
+        GLenum format;
+        if (nrComponents == 1)
+            format = GL_RED;
+        else if (nrComponents == 3)
+            format = GL_RGB;
+        else if (nrComponents == 4)
+            format = GL_RGBA;
+
+        glBindTexture(GL_TEXTURE_2D, textureID);
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, format == GL_RGBA ? GL_CLAMP_TO_EDGE : GL_REPEAT); // for this tutorial: use GL_CLAMP_TO_EDGE to prevent semi-transparent borders. Due to interpolation it takes texels from next repeat 
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, format == GL_RGBA ? GL_CLAMP_TO_EDGE : GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        stbi_image_free(data);
+    }
+    else
+    {
+        std::cout << "Texture failed to load at path: " << path << std::endl;
+        stbi_image_free(data);
+    }
+
+    return textureID;
 }
 
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
